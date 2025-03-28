7ce1a8971f3db8a30d5b8fceeaf842044c11a52f
lucas <lucrivdev@gmail.com>
2025-03-28 10:35:31 -0500

    First commit

---
+++ src/test/java/com/lucrivdev/tapioca/TapiocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucrivdev.tapioca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapiocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
